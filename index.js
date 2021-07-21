06830071e2336d798d6928a5175374797c83a567
Jeshurun Hembd <jjhembd@gmail.com>
2021-07-21 16:37:15 -0500

    Update ESLint config, and clean up

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ initZeroTimeouts();
 function initZeroTimeouts() {
   // setTimeout with true zero delay. https://github.com/GlobeletJS/zero-timeout
   const timeouts = [];
-  var taskId = 0;
+  let taskId = 0;
 
   // Make a unique message, that won't be confused with messages from
   // other scripts or browser tabs
   const messageKey = "zeroTimeout_$" + Math.random().toString(36).slice(2);
 
   // Make it clear where the messages should be coming from
-  const loc = window.location;
-  var targetOrigin = loc.protocol + "//" + loc.hostname;
-  if (loc.port !== "") targetOrigin += ":" + loc.port;
+  const { protocol, hostname, port } = window.location;
+  let targetOrigin = protocol + "//" + hostname;
+  if (port !== "") targetOrigin += ":" + port;
 
   // When a message is received, execute a timeout from the list
   window.addEventListener("message", evnt => {
     if (evnt.source != window || evnt.data !== messageKey) return;
     evnt.stopPropagation();
 
-    let task = timeouts.shift();
+    const task = timeouts.shift();
     if (!task || task.canceled) return;
     task.func(...task.args);
   }, true);
@@ -33,7 +33,7 @@ function initZeroTimeouts() {
   };
 
   window.clearZeroTimeout = function(id) {
-    let task = timeouts.find(timeout => timeout.id === id);
+    const task = timeouts.find(timeout => timeout.id === id);
     if (task) task.canceled = true;
   };
 }
